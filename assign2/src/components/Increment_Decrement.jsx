67128d872b514eb2cc6dd12a04268793613c3c00
AnyiC <anyichen212@gmail.com>
2023-06-15 14:20:06 -0400

    add Increment_Decrement file

---
+++ assign2/src/components/Increment_Decrement.jsx
@@ -0,0 +1,17 @@
+import React, {Component, ReactPropTypes} from "react";
+
+class Increment_Decrement extends Component {
+    constructor(props){
+
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default Increment_Decrement;
+
